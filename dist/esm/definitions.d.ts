721f0962549781484fe783ad49197f6eec0dd524
Shudhanshu Choudhary <shudhanshu.choudhary@appnox.ai>
2025-07-09 17:17:14 +0530

    feat: remove the dist/ from the gitignore

---
+++ dist/esm/definitions.d.ts
@@ -0,0 +1,18 @@
+export interface StepData {
+    steps: number;
+}
+export interface PermissionResult {
+    granted: boolean;
+}
+export interface DateRange {
+    start: number;
+    end: number;
+}
+export interface PedometerPlugin {
+    checkPermission(): Promise<PermissionResult>;
+    start(): Promise<void>;
+    stop(): Promise<void>;
+    getStepsBetween(options: DateRange): Promise<StepData>;
+    addListener(eventName: 'stepUpdate', listenerFunc: (data: StepData) => void): Promise<any>;
+    removeAllListeners(): Promise<void>;
+}
---
+++ dist/esm/index.js
@@ -0,0 +1,5 @@
+import { registerPlugin } from '@capacitor/core';
+const Pedometer = registerPlugin('Pedometer');
+export * from './definitions';
+export { Pedometer };
+//# sourceMappingURL=index.js.map
